b69d37a523813484c833d0d1c90d3d311ecad171
joy2323 <joyjoel2011@gmail.com>
2020-12-01 13:33:01 -0800

    added online database

--- config.php
+++ config.php
@@ -2,7 +2,9 @@
 session_start();
 
 //connect to the database...
-$conn = mysqli_connect("localhost", "root", "", "blog-php");
+// $conn = mysqli_connect("localhost", "root", "", "blog-php");
+$conn = mysqli_connect("db4free.net", "todoapp2020", "todoapp2020", "blog-php");
+
 
 if (!$conn){
     die("Error connectibg to database: " . mysqli_connect_error());
